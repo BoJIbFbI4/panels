c87c67af4f657cbc66e8a73fd0f19d778ccc92b1
greengnome <W99xkir77>
2017-01-06 19:12:32 +0200

    bug fix in login animation process

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,40 +2,49 @@ angular.module('panelsApp')
     .controller('AuthCtrl', ['$rootScope', '$scope', '$http', '$state', '$timeout', function ($rootScope, $scope, $http, $state, $timeout) {
 
         $scope.isLogin = false;
-        $scope.loginProcess = false;
         $rootScope.headerTitle = "panels";
         $rootScope.authorizationData = "";
         $rootScope.layout = "";
+        $scope.loginProcess = false;
 
 
         $scope.getLogin = function () {
-            $scope.loginProcess = true;
             var url = $rootScope.url + "/common/login";
             var authorizationData = btoa($scope.login + ":" + $scope.pass);
             $rootScope.authorizationData = authorizationData;
             var config = {headers: {"Authorization": "Basic " + authorizationData}};
 
-            $http.get(url, config).success(function (response) {
-                $scope.id = response.id;
-                $rootScope.type = response.type;
+            $scope.loginProcess = true;
+            console.log('loginProcess до гет запроса: ',$scope.loginProcess);
+            return $http.get(url, config)
+                .success(function(response) {
+                    $scope.id = response.id;
+                    $rootScope.type = response.type;
 
-                if (response.type == "ADMIN") {
-                    $state.go('companies')
-                }
-                if (response.type == "MANAGER") {
-                    $rootScope.alerts = response.supervisoryAlerts;
-                    $state.go('projects', {managerID: $scope.id})
-                }
+                    if (response.type == "ADMIN") {
+                        $state.go('companies')
+                    }
+                    if (response.type == "MANAGER") {
+                        $rootScope.alerts = response.supervisoryAlerts;
+                        $state.go('projects', {managerID: $scope.id})
+                    }
 
-                $scope.isLogin = true;
-                $scope.loginProcess = false;
-
-            }).error(function (response){
-                if ($scope.loginProcess) $scope.loginProcess = false;
-                $scope.login = "";
-                $scope.pass = "";
-                $scope.alert = "wrong login or password";
-            })
+                    $scope.isLogin = true;
+                    $scope.loginProcess = false;
+                    console.log('loginProcess в конце success гет запроса: ',$scope.loginProcess);
+                })
+                .error(function(data, status) {
+                    $scope.login = "";
+                    $scope.pass = "";
+                    $scope.alert = "wrong login or password";
+                    $scope.loginProcess = false;
+                    console.log('loginProcess в конце error гет запроса: ',$scope.loginProcess);
+                })
+                .finally(function() {
+                    $scope.loginProcess = false;
+                    console.log("finally finished repos");
+                    console.log('loginProcess в конце finaly гет запроса: ',$scope.loginProcess);
+                });
         };
 
         $rootScope.logout = function () {
